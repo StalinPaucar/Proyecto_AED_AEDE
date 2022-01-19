1627200a828a500d89574f6be8127646ca08c15e
StalinPaucar <stalinpaucar1103@hotmail.com>
2022-01-19 15:42:02 -0500

    add all

--- init.R
+++ init.R
@@ -2,21 +2,7 @@
 #
 # Example R code to install packages if not already installed
 #
-#helpers.installPackages("rgdal")
-#helpers.installPackages("gstat")
-#helpers.installPackages("gridExtra")
-#helpers.installPackages("ggpubr")
-
-my_packages = c("shiny","shinyWidgets", "shinythemes", "shinycssloaders",
-                "openxlsx", "lubridate", "tidyverse", "openair", "stringi",
-                "maptools", "modeest", "moments", "hms", "ggplot2", "ggseas", 
-                "viridis", "shadowtext", "plotly","ggpubr",
-                "Metrics", "nortest")
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing))
+helpers.installPackages("rgdal","gstat","shiny","shinyWidgets", "shinythemes", "shinycssloaders",
+                        "openxlsx", "lubridate", "tidyverse", "openair", "stringi",
+                        "maptools", "modeest", "moments", "hms", "ggplot2", "ggseas", 
+                        "viridis", "shadowtext","plotly","ggpubr","Metrics", "nortest")
